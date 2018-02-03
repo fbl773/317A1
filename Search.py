dafefe509c2f00c12c0f07e91810356efaac2a73
DevinFortin <daf165@mail.usask.ca>
2018-02-03 13:24:38 -0600

    Changed isQueueEmpty to isEmpty and made a Depth First search algortihm and a recursive helper for it.

--- Search.py
+++ Search.py
@@ -7,22 +7,53 @@ import Problem
 #Queue for BFS
 queue = []
 
-def isQueueEmpty(queue):
+def isEmpty(list):
 	return queue == []
 
 """
-Breadth first Search of the search states to find the goal state if it exists
+Breadth First Search of the search states to find the goal state if it exists
 :param: startState the state at which the search is starting from
 :param: problem the object for the current problem
 :returns: the state that is found to meet the goal state or nothing if the goal is not found.
 """
 def BFS( startState, problem):
 	queue.extend(Problem.getSuccessors(startState, problem))
-	while not isQueueEmpty(queue):
+	while not isEmpty(queue):
 		state = queue.pop()
 		if Problem.isGoal(state, problem):
 			return state
 		else:
 			queue.extend(Problem.getSuccessors(state, problem))
 	return None
+
+#Stack for DFSs
+stack = []
+
+"""
+Depth first search  of states to find the goal state if it exists
+:param: startState the state at which the search is starting from
+:param: problem the object for the current problem
+:returns: the state that is found to meet the goal state or nothing if the goal is not found.
+"""
+def DFS(startState, problem):
+	if Problem.isGoal(startState, problem):
+		return startState
+	stack.extend(Problem.getSuccessors(startState, problem))
+	return DFSRec(stack, problem)
+
+
+"""
+Recursive Helper for Depth first search
+:param: stack of the states in the problem
+:param: problem the object for the current problem
+:returns: the state that is found to meet the goal or nothing if it does not
+"""
+def DFSRec(stack, problem):
+	if isEmpty(stack):
+		return None
+	else:
+		state = stack.pop()
+		if Problem.isGoal(state):
+			return state
+		stack.extend(Problem.getSuccessors(state, problem))
 
