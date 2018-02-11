2fb5aab910b5709e089a325f5fedfbc91b9f17f1
= <fbl773@mail.usask.ca>
2018-02-11 14:51:03 -0600

    Testing is working. renamed some stuff. Made the 1D problem just a case of the two D problem where y coordinate is 0.

--- Search.py
+++ Search.py
@@ -13,8 +13,6 @@ queue = collections.deque([])
 def isQueueEmpty(queue):
 	return len(queue) == 0
 
-def isEmpty(aList):
-	return aList == []
 
 """
 Breadth First Search of the search states to find the goal state if it exists
@@ -46,7 +44,7 @@ def BFS(startState, problem):
 """
 A Shameless copy of BFS where the oneD methods are replaced with their twoD counterparts.
 """
-def BFSTwoD(startState,problem):
+def BFSTD(startState,problem):
 	MAXQUEUE = 0
 	NODESCREATED = 1
 	queue.append(startState)
@@ -65,57 +63,18 @@ def BFSTwoD(startState,problem):
 		else:
 			queue.extend(problem.getSuccessorsTD(state))
 	
-"""
-Depth first search  of states to find the goal state if it exists
-:param: startState the state at which the search is starting from
-:param: problem the object for the current problem
-:returns: the Path of the search the  number of nodes explored and the maximum size the queue was at any point
-		if the search fails the path will be empty
-"""
-def DFS(startState, problem):
-	stack = []
-	#Records the largest size the stack reaches
-	MAXSTACK = 0
-	#Records the number of nodes created
-	NODESCREATED = 1
-
-	if problem.isOneProbGoal(startState):
-		return ([startState], NODESCREATED, MAXSTACK)
-
-	newNodes = problem.getOneProbSuccessors(startState)
-	NODESCREATED += len(newNodes)
-	stack.extend(newNodes)
-	if MAXSTACK < len(stack):
-		MAXSTACK = len(stack)
-
-	newPathFlag = False
-	while not isEmpty(stack):
-		if newPathFlag is True:
-			newPathFlag = False
-		state = stack.pop()
-		path.append(state)
-		if problem.isOneProbGoal(state):
-			return (state,NODESCREATED, MAXSTACK)
-		else:
-			temp = problem.getOneProbSuccessors(state)
-			NODESCREATED += len(temp)
-			stack.extend(temp)
-			if MAXSTACK < len(stack):
-				MAXSTACK = len(stack)
-			if len(temp) < 1:
-				newPathFlag = True
-	return (None, NODESCREATED, MAXSTACK)
 
 def runTests():
 	bannr = "\n********************************\n"
 	
-	src = 0.5	
-	dst = 1.0
+	src = Problem.coordinate(0.5,0)	
+	dst = Problem.coordinate(1.0,0)
+	origin = Problem.coordinate(0,0)
 	
 	#Intializing objects	
 	prob = Problem.Problem(dst,src,1,1,1)
-	startState = Problem.ProblemStateWithRef(0,src,False,0,None)
-	print ("Source: ",src," Dest: ",dst)
+	startState = Problem.ProblemStateWithRef(origin,src,False,0,None)
+	print ("Source: ",src.toString()," Dest: ",dst.toString())
 	
 	#Check Objects
 	print ("Problem: ", prob.toString(),'\n')
@@ -125,32 +84,33 @@ def runTests():
 
 	#Testing DFS NOT IN THIS VERSION
 	print ("TESTING DFS",bannr)	
-	dfsStack = DFS(startState,prob)
-	print("Type of dfsStack: ", type(dfsStack))
-	print("Within it is: ", dfsStack)
-	print("And that is: ", dfsStack.toString())
+
+	# dfsStack = DFS(startState,prob)
+# 	print("Type of dfsStack: ", type(dfsStack))
+# 	print("Within it is: ", dfsStack)
+# 	print("And that is: ", dfsStack.toString())
 
 	print (bannr)
 
 	#Testing BFS
 	print ("TESTING BFS",bannr)
-	bfsQueue = BFS(startState,prob)
+	bfsQueue = BFSTD(startState,prob)
 	print("Type of bfsQueue: ",type(bfsQueue))
-	print("WIthin it is: ", type(bfsQueue[0]))
+	print("WIthin it is: ", bfsQueue)
 	print("And that is: ", bfsQueue[0])
 	print("Which contains: ", type(bfsQueue[0][0]))
 
 
 	print ("TEST BFS",bannr)
-	ResultsTup = BFS(startState,prob)
+	ResultsTup = BFSTD(startState,prob)
 
-
+	for items in ResultsTup[0]:
+		print(items.toString())
 	
 	#Two-D testing####################################
 	print(bannr,"Testing 2D",bannr)
 
 	#Defining Points
-	origin = Problem.coordinate(0,0)
 	src = Problem.coordinate(0.5,0.5)
 	dst = Problem.coordinate(1.0,1.0)
 	print ("TESTPOINTS: \n","Src: ",src.toString(),"\n Dest: ", dst.toString())
@@ -162,8 +122,8 @@ def runTests():
 	print ("State: ", startState2.toString())
 
 	#Testing Search
-	print("TESTING BFS",bannr)
-	bfsQueue2 = BFSTwoD(startState2,prob2)
+	print("TESTING 2D BFS",bannr)
+	bfsQueue2 = BFSTD(startState2,prob2)
 	print ("Within BFSTree is: ",bfsQueue2)
 	print ("And that is: ", bfsQueue2[0])
 	print ("Which contains: ", type(bfsQueue2[0][0]))
@@ -174,6 +134,8 @@ def runTests():
 	
 	
 	return
+
+	
 """
 	timedTest
 	a test function to track the times of the functions in this file
@@ -181,22 +143,26 @@ def runTests():
 	:returns nothing
 """
 def timedTest():
-	testProblem = Problem.Problem(0.5, 1.0)
-	testState = Problem.ProblemStateWithRef(0, 1.0, False, 0)
+	origin = Problem.coordinate(0,0)
+	src1 = Problem.coordinate(0.5,0)
+	dest1 = Problem.coordinate(1.0,0)
+	
+	testProblem = Problem.Problem(dest1,src1,1,1,1)
+	testState = Problem.ProblemStateWithRef(origin, 1.0, False, 0,None)
 	start = timer()
-	bfs = BFS(testState, testProblem)
+	bfs = BFSTD(testState, testProblem)
 	end = timer()
-	print("Time taken to run Breadth first search: ", end - start)
+	print("Time taken to run Breadth first search 1D: ", end - start)
 
-	src = Problem.coordinate(0.5, 0.5)
-	dest = Problem.coordinate(1.0, 1.0)
+	src2 = Problem.coordinate(0.5, 0.5)
+	dest2 = Problem.coordinate(1.0, 1.0)
 
-	tDProb = Problem.Problem(dest, src, 1, 1, 1)
-	tDStart = Problem.ProblemStateWithRef(Problem.coordinate(0, 0), src, False, 0, None)
+	tDProb = Problem.Problem(dest2, src2, 1, 1, 1)
+	tDStart = Problem.ProblemStateWithRef(Problem.coordinate(0,0), src2, False, 0, None)
 	# timing the one problem on 2D
 	start = timer()
 	bfs2 = BFSTD(tDStart, tDProb)
 	end = timer()
-	print("Time taken to run Breadth first search: ", end - start)
+	print("Time taken to run Breadth first search2D: ", end - start)
 
 	#potentially add code for the general function here too.
